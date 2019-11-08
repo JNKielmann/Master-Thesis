f06b56950cf0c4a86cb1184b08eb81db12269e8f
Jan Kielmann <jan.kielmann@sap.com>
2019-11-08 09:02:55 +0100

    Add grid search for bm25 k1 parameter

--- paper_retrieval/bm25_retrieval.py
+++ paper_retrieval/bm25_retrieval.py
@@ -1,22 +1,24 @@
 import pickle
-from typing import List
 import pandas as pd
-from gensim.summarization.bm25 import BM25
+from gensim.summarization import bm25
 
 from preprocessing import apply_pipeline, Corpus
 
 
 class BM25Retrieval:
-    def __init__(self, corpus: Corpus):
+    def __init__(self, corpus: Corpus, k1: float = 1.5, b: float = 0.75):
         self.ids = pd.Series(corpus.ids, name="id")
-        self.bm25_model = BM25(corpus.data)
+        self.bm25_model = bm25.BM25(corpus.data)
         self.pipeline = corpus.pipeline
+        self.k1 = k1
+        self.b = b
 
     def get_ranked_documents(self, query: str) -> pd.DataFrame:
         query = apply_pipeline(query, self.pipeline)
         query = query.split(" ")
-        # print(query)
         df = pd.DataFrame(self.ids)
+        bm25.PARAM_K1 = self.k1
+        bm25.PARAM_B = self.b
         df["score"] = self.bm25_model.get_scores(query)
         df.sort_values(by="score", ascending=False, inplace=True)
         return df[df["score"] > 0]
