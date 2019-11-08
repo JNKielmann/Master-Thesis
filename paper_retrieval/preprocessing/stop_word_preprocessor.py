c17ac543ad974dfeeeeacebb8fffe33afe5e3d04
Jan Kielmann <jan.kielmann@sap.com>
2019-11-08 09:02:05 +0100

    Add stop word preprocessing

---
+++ paper_retrieval/preprocessing/stop_word_preprocessor.py
@@ -0,0 +1,10 @@
+from spacy.lang.en.stop_words import STOP_WORDS
+
+
+class StopWordPreprocessor():
+    def __init__(self):
+        self.name = "NoStopWords"
+
+    def __call__(self, text: str):
+        return " ".join([word for word in text.split(" ") if
+                         word not in STOP_WORDS and len(word) > 3])
