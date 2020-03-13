40727580bc687c11b3599c404dbe069f89ed1f3c
Jan Kielmann <jan.kielmann@sap.com>
2020-03-13 16:42:27 +0100

    Add coauthor modifier

---
+++ paper_retrieval/expert_voting/author_score_modifiers/co_author_modifier.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from expert_voting.author_score_modifiers.author_score_modifier import AuthorScoreModifier
+from expert_voting.scored_expert import ScoredExpert
+
+
+class CoAuthorModifier(AuthorScoreModifier):
+    def __init__(self, doc_info, alpha):
+        self.alpha = alpha
+        self.co_author_matrix = {}
+        for paper in doc_info.values():
+            for author in paper["authors"]:
+                if str(author["id"]) not in self.co_author_matrix:
+                    self.co_author_matrix[author["id"]] = {}
+                for co_author in doc_info["authors"]:
+                    if co_author["id"] != author["id"]:
+                        co_authors = self.co_author_matrix[author["id"]]
+                        if str(co_author["id"]) not in co_authors:
+                            co_authors[str(co_author["id"])] = 0
+                        co_authors[str(co_author["id"])] += 1
+
+    def modify(self, input_list: List[ScoredExpert]) -> List[ScoredExpert]:
+        result = []
+        for author in input_list:
+            additional_score = 0
+            C = 0
+            for co_author in input_list:
+                if co_author.id != author.id:
+                    co_author_papers = self.co_author_matrix[str(author.id)].get(
+                        str(co_author.id), 0)
+                    additional_score += co_author_papers * co_author.score
+                    C += co_author_papers
+            new_score = author.score + self.alpha * (additional_score/C)
+            result.append(author.change_score(new_score))
