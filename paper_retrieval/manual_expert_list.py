77233976da63d72614d169f5edb5ceae3075722f
Jan Kielmann <jan.kielmann@sap.com>
2020-03-13 14:11:00 +0100

    Update manual expert list

---
+++ paper_retrieval/manual_expert_list.py
@@ -0,0 +1,154 @@
+manual_expert_list = {
+    "internet of things": [
+        673846798,  # Michael Beigl
+        2130664820,  # Till Riedel
+        2130344165, # Jörg Henkel
+        2241467651, # Lars Bauer
+        1974079823, # Farzad Samie
+        2078281586,  # Matthias Budde
+        2246826143, # Mehdi B. Tahoori
+        102396157, # Martina Zitterbart
+        2018480418, # Tobias Käfer
+
+        # 2536964599,  # Anja Exler
+        # 2555467802,  # erik pescara
+    ],
+    "blockchain": [
+        2610667053,  # benedikt notheisen (not at kit, at kit in gs)
+        2419283475,  # florian glaser (not at kit, at kit in gs)
+        # 1808354562, # florian hawlitschek (not at kit, not at kit in gs)
+        2224817115,  # till neudecker (not at kit, at kit in gs)
+        899792304,  # christof weinhardt
+        2622468455,  # esther mengelkamp (not at kit, at kit in gs)
+        2058489235,  # Hannes Hartenstein
+        2021103267,  # Ali Sunyaev
+        2587321452,  # stefan seebacher
+        # 2898611480, # matthias grundmann (at kit, not in gs)
+        2922954189,  # Niclas Kannengießer
+        2607646339,  # Sebastian Lins
+        1245553041,  # Tobias Dehling
+    ],
+    "natural language processing": [
+        2061074560,  # alex waibel
+        # 2045275999, # jan niehues (not at kit, not at kit in gs)
+        280384801,  # Sebastian Stüker
+        2101077920,  # markus muller
+        2170753228,  # Thanh-Le Ha
+        2759924440,  # Ngoc-Quan Pham
+        2790065202,  # Stefan Constantin
+        2105886198,  # Rudi Studer
+        268444295,  # alexander maedche
+        2165062152,  # Matthias Sperber
+        2791786201,  # Michael Färber
+        2164999476,  # Harald Sack
+        2231152044,  # Sebastian Weigelt
+        2578689274,  # Tobias Hey
+        2074083528,  # Walter F. Tichy
+        2800040021,  # Abel Elekes
+    ],
+    "autonomous driving": [
+        2056797944,  # christoph stiller
+        2116123198,  # Carlos, Fernández López
+        2156656717,  # martin lauer
+        2563466229,  # Omer Sahin Tas
+        1962891380,  # michael flad
+        1961238654,  # frank gauterin
+        2490080048,  # michael frey
+        2293248550,  # Eric Sax
+        2498022428,  # Raphael Pfeffer
+        2124202115,  # soren hohmann
+        2947663597,  # nathalie brenner
+        2246613708,  # andreas lauber
+        2626537028,  # simon rothfus
+        2962980503,  # Marius Zollner
+        2746570896,  # Maximilian Naumann
+        2810059395,  # Annika Meyer
+
+    ],
+    "humanoid robot": [
+        1047662447,  # tamim asfour
+        2614726016,  # albert albers
+        2161631798,  # mirko wachter
+        2026820270,  # r dillmann
+        332721774,  # rainer stiefelhagen
+        1967175795,  # torsten kroger
+        2570474579,  # Simon Ottenhaus
+        2637288922,  # Raphael Grimm
+        2228623559,  # Markus Grotz
+        2772428913,  # Fabian Paus
+        2571366741,  # Lukas Kaul
+        2588863116,  # Samuel Rader
+        2248512682,  # Pascal Weiner
+        2565089218,  # You Zhou
+        2910150326,  # julia starke
+    ],
+    # "deep learning for computer vision": [
+    #
+    # ],
+    "evolutionary algorithm": [
+        2056122262,  # peter sanders
+        2036573014,  # Hartmut Schmeck
+        2132999805,  # Pradyumn Kumar Shukla
+        2663770362,  # Sebastian Lamm
+        2077764046,  # karluwe stucky
+        2104163988,  # wilfried jakob
+        2614726016,  # albert albers
+
+    ],
+    "remote sensing": [
+        2023722769,  # johannes orphal
+        58760226,  # Sebastian Schmidtlein
+        2522172188,  # Michael Ewald
+        2018353418,  # Fabian Ewald Fassnacht
+        2021485836,  # Teja Kattenborn
+        2571998307,  # Javier Lopatin
+        2615113834,  # Julia, Fuchs
+        1975061826,  # Udo Grabowski
+        2037113860,  # Frank-M. Göttsche
+        2811921626,  # Tobias Kerzenmacher
+        2226192829,  # Hansjörg Kutterer
+        2230266206,  # matthias schneider
+        2199169488,  # Miriam Sinnhuber
+        2171345698,  # Andreas Braun
+        1915376222,  # Werner Wiesbeck
+        2132480425,  # Frank Hase
+        2471318715,  # Stefan Hinz
+        2314048819,  # Sina Keller
+        2570136068,  # Jan Cermak
+        2261027585,  # martin weinmann
+        346619331,  # Boris Jutzi
+        2141378911,  # Andreas Wieser
+    ],
+    # "action recognition": [
+    #     332721774, # Rainer Stiefelhagen
+    #     2226325802, # Alina Roitberg
+    #
+    #
+    # ],
+    # "digital signatures": [
+    #
+    # ],
+    # "quantum cryptography": [
+    #
+    # ],
+    # "cellular automaton": [
+    #     2464108834, # Simon Wacker
+    #     183621591, # Thomas Worsch
+    # ],
+    # "electronic voting": [
+    #     135566254, # Jörn Müller-Quade
+    #     2226726414, # Jochen Rill
+    #     165695016, # melanie volkamer
+    #     2038785220, # Bernhard Beckert
+    #
+    # ],
+    # "spiking neural network": [
+    #
+    # ],
+    # "kalman filter": [
+    #     13246170, # Uwe D. Hanebeck
+    #     2165996130, # Benjamin Noack
+    #     2056797944, # Christoph Stiller
+    #     2228035992, # Florian Pfaff
+    # ]
+}
