f00bce9ecb2c4d2ee756b8491761ba489896f1f8
Jan Kielmann <jan.kielmann@sap.com>
2020-03-22 13:24:15 +0100

    Add kompetenzpool dataset

---
+++ paper_retrieval/kompetenzpool_expert_list.py
@@ -0,0 +1,76 @@
+kompetenzpool_expert_list = {
+    "Internet of Things": [
+        2241467651,  # Lars Bauer
+        673846798,  # Michael Beigl
+        2236413454
+    ],
+    "blockchain": {
+        1245553041,  # Tobias Dehling
+        2021103267,  # Ali Sunyaev
+    },
+    "natural language processing": {
+        2794237920,  # Constantin, Stefan
+        2578689274,  # Tobias Hey
+    },
+    "autonomous driving": {
+        2490080048,  # michael frey
+        2156656717,  # martin lauer
+    },
+    "humanoid robot": {
+        1047662447,  # tamim asfour
+        2570474579,  # Simon Ottenhaus
+        2978684628,  # Isabel Patzer
+        2772428913,  # Fabian Paus
+        2588863116,  # Samuel Rader
+        2910150326,  # julia starke
+        2248512682,  # Pascal Weiner
+        2565089218,  # You Zhou
+    },
+    "evolutionary algorithm": {
+        2104163988,  # wilfried jakob
+        2036573014,  # Hartmut Schmeck
+    },
+    "remote sensing": [
+        1983767936,  # Stefan Emeis
+        2471318715,  # Stefan Hinz
+        2636005816, # Sören Johansson
+        2889396847, # Roland Stirnberg
+    ],
+    "cellular automaton": [
+        183621591,  # Thomas Worsch
+        2484531368, # augusto modanese
+    ],
+    "electronic voting": [
+        2038785220,  # Bernhard Beckert
+    ],
+    "kalman filter": [
+        154988935, # Bäuerle, Nicole
+        2165996130,  # Benjamin Noack
+        2228035992,  # Florian Pfaff
+    ],
+    "software architecture": [
+        2145590652,  # Anne Koziolek
+    ],
+    "deflectometry": [
+        2147812722, # Stephan Allgeier
+    ],
+    "robot grasping": [
+        1047662447,  # tamim asfour
+        2565089218,  # You Zhou
+    ],
+    "ubiquitous computing": [
+        673846798,  # Michael Beigl
+    ],
+    "human-computer interaction": [
+        673846798,  # Michael Beigl
+        332721774,  # Rainer Stiefelhagen
+    ],
+    "Digital Health": [
+        2021103267,  # Ali Sunyaev
+    ],
+    "fuel cells": [
+        2254823364,
+        2171498847,
+        1723818860,
+    ]
+}
