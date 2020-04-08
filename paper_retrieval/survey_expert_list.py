06308b12ebb472806e67c7f462bc44b7a7bcf6a8
root <jnkielmann@googlemail.com>
2020-04-08 14:53:52 +0000

    Move survery expert list

---
+++ paper_retrieval/survey_expert_list.py
@@ -0,0 +1,25 @@
+survey_expert_list = {
+    "humanoid robot": [
+        1047662447,  # tamim asfour
+        1305265972,  # r dillmann
+        2570474579,  # Simon Ottenhaus
+    ],
+    "natural language processing": [
+        2061074560,  # alex waibel
+        2074083528,  # Walter F. Tichy
+        280384801,  # Sebastian Stüker
+        2578689274,  # Tobias Hey
+        2170753228,  # Thanh-Le Ha
+        2794237920, # Stefan Constantin
+    ],
+    "internet of things": [
+        673846798,  # Michael Beigl
+        102396157,  # Martina Zitterbart
+        2130664820,  # Till Riedel
+    ],
+    "evolutionary algorithm": [
+        2056122262,  # peter sanders
+        2132999805,  # Pradyumn Kumar Shukla
+        2036573014,  # Hartmut Schmeck
+    ]
+}
